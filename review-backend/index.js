10dede473b1053f361e7b0c62d8152ff589ad643
root <root@DESKTOP-N0Q3S1A>
2023-05-29 05:11:51 +0200

    version1

---
+++ review-backend/index.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+import app from "./server.js";
+import {MongoClient} from "mongodb";
+import ReviewsDAO from "./dao/reviewsDAO.js";
+
+dotenv.config();
+
+
+const mongo_username = process.env['MONGO_USERNAME'];
+const mongo_password = process.env['MONGO_PASSWORD'];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.pma6dym.mongodb.net/?retryWrites=true&w=majority`;
+
+const port = 8000;
+
+MongoClient.connect(
+    uri,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
+    })
+    .then(async client => {
+        await ReviewsDAO.injectDB(client);
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`);
+        });
+    })
+    .catch(err => {
+        console.error(err.stack);
+        process.exit(1);
+    });
